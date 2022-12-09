5207ddcd3e6dd1317704bef7edec58321e7fe42c
Queiroga <victorqueirogacarvalho@gmail.com>
2022-12-08 21:31:14 -0300

    feat: configurating component for a generic project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,10 @@
 function Project(props){
     return(
         <>
-            <div class='' style={{ width: props.width}}>
-                <img src={props.src} alt="" />
+            <div>
+                <img class='mb-[15px] object-cover rounded-[10px]' style={{ width: props.width, height: props.height}} src={props.src} alt="" />
+                <label class='font-[14px]'>{props.title}</label> <br />
+                <label class='text-projectgrey font-[14px]'>{props.date}</label>
             </div>
         </>
     )
