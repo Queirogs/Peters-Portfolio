6281210cb9f9c2e8427a94b2dee054a8d5a0ff55
Queiroga <victorqueirogacarvalho@gmail.com>
2022-12-08 21:29:32 -0300

    feat: adding project colours and adding arrow moving animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-      color: {
-        'project-blue':'#0904FF',
-        'project-white':'#f1f1f1'
+      colors: {
+        'projectblue':'#0904FF',
+        'projectwhite':'#f1f1f1',
+        'projectgrey':'#6A6A6A'
       },
       animation: {
         'spin-slow': 'spin 7s linear infinite',
+        'wiggle': 'wiggle 3s ease-in-out infinite'
       },
       fontFamily: {
         'albert': ['albert-sans', 'sans-serif']
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'translateY(-20px)' },
+          '50%': { transform: 'translateY(20px)' },
+        }
       }
     },
   },
